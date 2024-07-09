a0e21f3881513e37f0cee30d098e0dc8a5d1cfb5
Ryosuke <piesuke0727@gmail.com>
2024-07-09 10:02:54 +0900

    today

---
+++ bonjin/abc195_b.py
@@ -0,0 +1,16 @@
+A,B,W=map(int, input().split())
+
+W_g=W*1000
+
+min_ans = 10**15
+max_ans = -10**15
+for X in range(1,W_g // A + 1):
+    if A*X<=W_g<=B*X:
+        min_ans=min(min_ans,X)
+        max_ans=max(max_ans,X)
+
+if min_ans==10**15:
+    print("UNSATISFIABLE")
+
+else:
+    print(min_ans,max_ans)
